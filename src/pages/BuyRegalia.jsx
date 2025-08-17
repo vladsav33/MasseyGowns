da8ee0e0459a9a4188243b1eeae278c913f9367a
Nethmi Weththasinghe <nwetthasinha@gmail.com>
2025-08-17 13:42:57 +1200

    Create buy and hire pages

---
+++ src/pages/BuyRegalia.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import "./BuyRegalia.css";
+
+function BuyRegalia() {
+  return (
+    <div>
+      <Navbar />
+
+
+      <Footer />
+    </div>
+  );
+}
+
+export default BuyRegalia;
