11a64bf7c7d21fe2f2b4d1d35907590433fce6a5
Vladimir Savitskiy <vladsav33@yandex.ru>
2025-09-15 18:08:19 +1200

    Removed drop down menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,71 +17,71 @@ function Navbar() {
             HIRE REGALIA
           </Link>
 
-          <section className="dropdown_panel">
-            <div className="dropdown_casual">
-              <h4>Casual Hire for Photos</h4>
-              <ul>
-                <li>
-                  <a href="#">Certificate</a>
-                </li>
-                <li>
-                  <a href="#">Diploma / Graduate Diploma / Post Grad Diploma</a>
-                </li>
-                <li>
-                  <a href="#">Bachelor Degree</a>
-                </li>
-                <li>
-                  <a href="#">Master Degree</a>
-                </li>
-                <li>
-                  <a href="#">PhD Degree</a>
-                </li>
-                <li>
-                  <a href="#">
-                    Doctoral Degree (DEd, DBusAdmin, DClinPsych, DSW)
-                  </a>
-                </li>
-                <li>
-                  <a href="#">Higher Doctoral Degree</a>
-                </li>
-              </ul>
-            </div>
-          </section>
+          {/*<section className="dropdown_panel">*/}
+          {/*  <div className="dropdown_casual">*/}
+          {/*    <h4>Casual Hire for Photos</h4>*/}
+          {/*    <ul>*/}
+          {/*      <li>*/}
+          {/*        <a href="#">Certificate</a>*/}
+          {/*      </li>*/}
+          {/*      <li>*/}
+          {/*        <a href="#">Diploma / Graduate Diploma / Post Grad Diploma</a>*/}
+          {/*      </li>*/}
+          {/*      <li>*/}
+          {/*        <a href="#">Bachelor Degree</a>*/}
+          {/*      </li>*/}
+          {/*      <li>*/}
+          {/*        <a href="#">Master Degree</a>*/}
+          {/*      </li>*/}
+          {/*      <li>*/}
+          {/*        <a href="#">PhD Degree</a>*/}
+          {/*      </li>*/}
+          {/*      <li>*/}
+          {/*        <a href="#">*/}
+          {/*          Doctoral Degree (DEd, DBusAdmin, DClinPsych, DSW)*/}
+          {/*        </a>*/}
+          {/*      </li>*/}
+          {/*      <li>*/}
+          {/*        <a href="#">Higher Doctoral Degree</a>*/}
+          {/*      </li>*/}
+          {/*    </ul>*/}
+          {/*  </div>*/}
+          {/*</section>*/}
         </li>
         <li className="has-dropdown">
           <Link to="/buyregalia" className="menu-link">
             BUY REGALIA
           </Link>
-          <section className="dropdown-panel">
-            <div className="buyfull">
-              <h4>Buy Full Regalia Sets</h4>
-              <ul className="drondown-list">
-                <li>
-                  <a href="#">Buy Bachelor Degree Set</a>
-                </li>
-                <li>
-                  <a href="#">Buy Master Degree Set</a>
-                </li>
-                <li>
-                  <a href="#">Buy PhD Degree Set</a>
-                </li>
-              </ul>
-            </div>
-            <div className="buyindividual">
-              <h4>Buy Individual Pieces</h4>
-              <ul className="drondown-list">
-                <li>
-                  <a href="#">Buy Gowns</a>
-                </li>
-                <li>
-                  <a href="#">Buy Hoods</a>
-                </li>
-                <li>
-                  <a href="#">Buy Headwears</a>
-                </li>
-              </ul>
-            </div>
-          </section>
+          {/*<section className="dropdown-panel">*/}
+          {/*  <div className="buyfull">*/}
+          {/*    <h4>Buy Full Regalia Sets</h4>*/}
+          {/*    <ul className="drondown-list">*/}
+          {/*      <li>*/}
+          {/*        <a href="#">Buy Bachelor Degree Set</a>*/}
+          {/*      </li>*/}
+          {/*      <li>*/}
+          {/*        <a href="#">Buy Master Degree Set</a>*/}
+          {/*      </li>*/}
+          {/*      <li>*/}
+          {/*        <a href="#">Buy PhD Degree Set</a>*/}
+          {/*      </li>*/}
+          {/*    </ul>*/}
+          {/*  </div>*/}
+          {/*  <div className="buyindividual">*/}
+          {/*    <h4>Buy Individual Pieces</h4>*/}
+          {/*    <ul className="drondown-list">*/}
+          {/*      <li>*/}
+          {/*        <a href="#">Buy Gowns</a>*/}
+          {/*      </li>*/}
+          {/*      <li>*/}
+          {/*        <a href="#">Buy Hoods</a>*/}
+          {/*      </li>*/}
+          {/*      <li>*/}
+          {/*        <a href="#">Buy Headwears</a>*/}
+          {/*      </li>*/}
+          {/*    </ul>*/}
+          {/*  </div>*/}
+          {/*</section>*/}
         </li>
         <li>
           <Link to="/faqs">FAQs</Link>
